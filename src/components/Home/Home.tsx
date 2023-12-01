bf020cb23cf4db376b57a2fbf5f4753d13354588
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-12-01 20:45:07 +0600

    "Create state with empty array. Create function with axiosApi.get request then create function for fetch data"

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,54 @@
 import '../../styles.css';
+import axiosApi from '../../axiosApi.ts';
+import {useState, useEffect} from 'react';
 
+interface Post {
+    date: string;
+    title: string;
+}
 
-const Home= () => {
+const Home = () => {
+    const [postList, setPostList] = useState<Post[]>([]);
 
+    const getPosts = async () => {
+        const response = await axiosApi.get('posts.json');
+        return response.data;
+    };
 
+    const fetchData = async () => {
+        try {
+            const postsObject = await getPosts();
+            const postsArray = Object.keys(postsObject).map(key => ({
+                id: key,
+                date: postsObject[key].posts.date,
+                title: postsObject[key].posts.title,
+            }));
 
-  return (
-    <div className="d-flex justify-content-center align-items-center mt-5">
-      <div className="d-flex flex-column border border-3 border-success rounded-2 p-3 post-size">
-      <span className="mb-3"><b><i>Created on: </i></b> 22.08.23 15:10</span>
-        <h4 className="mb-3">Some title post...</h4>
-        <button className="btn-read w-25 btn btn-success text-start ps-3">Read more</button>
-      </div>
-    </div>
-  );
+            setPostList(postsArray);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    });
+
+    return (
+        <>
+            {postList.map((post, index) => (
+                <div key={index} className="d-flex justify-content-center align-items-center mt-5">
+                    <div className="d-flex flex-column border border-3 border-success rounded-2 p-3 post-size">
+                        <span className="mb-3">
+                            <b><i>Created on: </i></b>{post.date}
+                        </span>
+                        <h4 className="mb-3">{post.title}</h4>
+                        <button className="btn-read w-25 btn btn-success text-start ps-3">Read more</button>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 };
 
 export default Home;
