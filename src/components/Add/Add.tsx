cf594171433c72262ff0f14b6a00270cbe75ce54
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-12-01 18:47:05 +0600

    Create new state 'loading'. Create post reuqest on 'onFormSumbit' to server use try/finally. Create component Spinner"

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,7 +1,9 @@
+import axiosApi from "../../axiosApi.ts";
 import React, {useCallback, useState} from 'react';
 import TypeWriter from '../../images/ic-typewriter.png';
-import '../../styles.css';
 import {Posts} from '../../types';
+import '../../styles.css';
+import Spinner from "../Spinner/Spinner.tsx";
 
 const Add: React.FC = () => {
     const [posts, setPosts] = useState<Posts>({
@@ -10,6 +12,8 @@ const Add: React.FC = () => {
         title: '',
         message: '',
     });
+
+    const [loading, setLoading] = useState(false);
 
     const inputChange = useCallback((event: React.ChangeEvent<HTMLInputElement & HTMLTextAreaElement>) => {
         const randomId = `${Math.random()}`;
@@ -23,48 +27,61 @@ const Add: React.FC = () => {
         }));
     }, []);
 
-
-    const onFormSubmit = (e: React.FormEvent) => {
+    const onFormSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        if (posts.title !== '' && posts.message !== '') {
-            setPosts({id: '', title: '', message: '', date: ''});
-        } else {
-            alert(`Title and message may not be empty. Set value!`);
+        setPosts({id: '', title: '', message: '', date: ''});
+        setLoading(true);
+
+        const PostsData = {
+            posts,
+        };
+
+        try {
+            await axiosApi.post('posts.json', PostsData);
+        } finally {
+            setLoading(false);
         }
-        console.log(posts);
     };
+
+    let form = (
+        <form onSubmit={onFormSubmit} className="w-25 bg-light p-3 rounded-3">
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="w-50 pb-3 mb-5 text-center border-2 border-bottom border-secondary">
+                    <img src={TypeWriter} alt="Typywriter icon"/>
+                </div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="title-input" className="form-label">Add title for your message</label>
+                <input
+                    name="title"
+                    onChange={inputChange}
+                    value={posts.title}
+                    type="text"
+                    className="form-control"
+                    id="title-input"
+                    placeholder="Example: Meeting at the business center" required/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="input-message" className="form-label">Input your message</label>
+                <textarea
+                    value={posts.message}
+                    name="message"
+                    onChange={inputChange}
+                    className="form-control"
+                    id="input-message"
+                    placeholder='Example:"Today I had a productive meeting with our partners from Europe"' required/>
+            </div>
+            <button type="submit" className="btn btn-success">Save</button>
+        </form>
+    );
+
+    if (loading) {
+        form = <Spinner/>;
+    }
 
     return (
         <div className="mt-5 d-flex justify-content-center align-items-center">
-            <form onSubmit={onFormSubmit} className="w-25 bg-light p-3 rounded-3">
-                <div className="d-flex justify-content-center align-items-center">
-                    <div className="w-50 pb-3 mb-5 text-center border-2 border-bottom border-secondary">
-                        <img src={TypeWriter} alt="Typywriter icon"/>
-                    </div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="title-input" className="form-label">Add title for your message</label>
-                    <input
-                        name="title"
-                        onChange={inputChange}
-                        value={posts.title}
-                        type="text"
-                        className="form-control"
-                        id="title-input"
-                        placeholder="Example: Meeting at the business center" required/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="input-message" className="form-label">Input your message</label>
-                    <textarea
-                        value={posts.message}
-                        name="message"
-                        onChange={inputChange}
-                        className="form-control"
-                        id="input-message"
-                        placeholder='Example:"Today I had a productive meeting with our partners from Europe"'/>
-                </div>
-                <button type="submit" className="btn btn-success">Save</button>
-            </form>
+            {form}
         </div>
     );
 };
