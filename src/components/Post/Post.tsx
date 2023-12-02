b0b12ec2a2d1fe532730fcc2ba7529ad7049325a
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-12-02 09:57:36 +0600

    Create logic for state edit

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,14 +1,18 @@
-import {NavLink, useParams} from 'react-router-dom';
-import { useState, useEffect } from "react";
+import React, {useState, useEffect, useCallback} from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
 import axiosApi from '../../axiosApi.ts';
 
 const Post = () => {
-    const { postId } = useParams();
+    const {postId} = useParams();
+    const navigate = useNavigate();
+
     const [post, setPost] = useState({
-        date: "",
-        title: "",
-        message: "",
+        date: '',
+        title: '',
+        message: '',
     });
+
+    const [edit, setEdit] = useState(false);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -19,8 +23,46 @@ const Post = () => {
                 console.error('Error fetching post data:', error);
             }
         };
+
         fetchPost();
+    }, [postId]);
+
+    const deletePost = async () => {
+        try {
+            await axiosApi.delete(`posts/${postId}.json`);
+            navigate('/');
+        } catch (error) {
+            console.error('Error deleting post:', error);
+        }
+    };
+
+    const editChange = useCallback((event: React.ChangeEvent<HTMLInputElement & HTMLTextAreaElement>) => {
+        const {name, value} = event.target;
+        setPost((prevPost) => ({
+            ...prevPost,
+            [name]: value,
+        }));
     }, []);
+
+
+    const saveEdit = async () => {
+        try {
+            await axiosApi.put(`posts/${postId}.json`, {
+                posts: post,
+            });
+            navigate('/');
+        } catch (error) {
+            console.error('Error saving post edits:', error);
+        }
+    };
+
+    const editPost = () => {
+        setEdit(true);
+    };
+
+    const cancelEdit = () => {
+        setEdit(false);
+    };
 
     return (
         <div className="d-flex justify-content-center align-items-center mt-5">
@@ -28,12 +70,37 @@ const Post = () => {
                 <span className="mb-3">
                     <b><i>Created on: </i></b>{post.date}
                 </span>
-                <h4 className="mb-3">{post.title}</h4>
-                <p>{post.message}</p>
-              <div className="d-flex justify-content-end gap-3">
-                <NavLink to={`/posts/${post.id}`} className="w-25 btn btn-danger text-center ps-3">Delete</NavLink>
-                <NavLink to={`/posts/${post.id}`} className="w-25 btn btn-primary text-center ps-3">Edit</NavLink>
-              </div>
+                {edit ? (
+                    <>
+                        <label>Edit Title:</label>
+                        <input
+                            type="text"
+                            name="title"
+                            value={post.title}
+                            onChange={editChange}
+                        />
+                        <label>Edit Message:</label>
+                        <textarea
+                            name="message"
+                            value={post.message}
+                            onChange={editChange}
+                        />
+                        <div className="d-flex justify-content-end gap-3 mt-4">
+                            <button onClick={saveEdit} className="btn btn-danger">Cancel</button>
+                            <button onClick={cancelEdit} className="btn btn-warning">Save</button>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <h4 className="mb-3">{post.title}</h4>
+                        <p>{post.message}</p>
+                        <div className="d-flex justify-content-end gap-3">
+                            <button onClick={deletePost} className="w-25 btn btn-danger text-center ps-3">Delete
+                            </button>
+                            <button onClick={editPost} className="w-25 btn btn-primary text-center ps-3">Edit</button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
